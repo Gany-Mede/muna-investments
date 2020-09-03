730a23eaed7020c2e5c1096d52cd18139970a5d3
Sohail Farooq <farsohail@hotmail.com>
2020-09-03 00:00:18 -0400

    commit

--- script.js
+++ script.js
@@ -76,50 +76,32 @@ function getData() {
       //create chart.js canvas
       //<canvas id="mychart" width="400" height="400"></canvas>
       //setAttribute  or      .width =
+      //create chart.js canvas
+      //<canvas id="mychart" width="400" height="400"></canvas>
+      //setAttribute  or      .width =
       let theChart = document.createElement("canvas");
+      theChart.src =
+        "https://cloud.iexapis.com/stable/stock/aapl/batch?types=chart&range=1m&last=10&token=pk_93c31736e1884c4d9585a581f9a431d6";
       theChart.style.width = "400";
       theChart.style.height = "400";
       cardBody.append(theChart);
       let cardChart = theChart.getContext("2d");
+
       var myChart = new Chart(cardChart, {
-        type: "bar",
+        type: "line",
         data: {
-          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          labels: ["January", "Febuary", "March"],
           datasets: [
             {
-              label: "# of Votes",
-              data: [12, 19, 3, 5, 2, 3],
-              backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ],
-              borderWidth: 1,
+              label: symbol,
+              data: [10, 35, 40],
+              // backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+              borderColor: ["rgb(7,7,7)"],
+              // borderWidth: 1,
             },
           ],
         },
-        options: {
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-        },
+        options: {},
       });
 
       //button logo
